1603ebaffb166a0d31fcae8e700406ac43920d98
joepasss <farwes1180@gmail.com>
2023-09-18 02:42:54 +0900

    webpack dev server configuration

--- webpack/webpack.dev.config.ts
+++ webpack/webpack.dev.config.ts
@@ -1,9 +1,30 @@
-import { Configuration } from "webpack";
+import path from "path";
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 import commonConfig from "./webpack.common.config";
 import { merge } from "webpack-merge";
 
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
 const prodConfig: Configuration = merge(commonConfig, {
   mode: "development",
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, ".."),
+    },
+    devMiddleware: {
+      index: "index.html",
+      writeToDisk: true,
+    },
+    client: {
+      overlay: true,
+    },
+    liveReload: true,
+  },
 });
 
 export default prodConfig;
