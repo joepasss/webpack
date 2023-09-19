19f87dba9906757785ba5804f9923520a16cd8e8
joepasss <farwes1180@gmail.com>
2023-09-19 13:42:19 +0900

    apply style-loader and css-loader

--- webpack/webpack.common.config.ts
+++ webpack/webpack.common.config.ts
@@ -3,6 +3,14 @@ import { Configuration } from "webpack";
 
 const commonConfig: Configuration = {
   entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "main.js",
