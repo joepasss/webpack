e66ef6b1974d38ecbfdbb3ac28ead6501c6e99f1
joepasss <farwes1180@gmail.com>
2023-09-19 15:44:52 +0900

    implement clean-webpack-plugin

--- webpack/webpack.common.config.ts
+++ webpack/webpack.common.config.ts
@@ -1,11 +1,13 @@
 import path from "path";
 import { Configuration } from "webpack";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 const commonConfig: Configuration = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "../dist"),
   },
+  plugins: [new CleanWebpackPlugin()],
 };
 
 export default commonConfig;
