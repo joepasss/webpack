e516d373965a5a63efeb031a91b79f21f34beb17
joepasss <farwes1180@gmail.com>
2023-09-19 14:58:04 +0900

    apply mini-css-extract-plugin

--- webpack/webpack.prod.config.ts
+++ webpack/webpack.prod.config.ts
@@ -1,9 +1,19 @@
 import { Configuration } from "webpack";
 import commonConfig from "./webpack.common.config";
 import { merge } from "webpack-merge";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const prodConfig: Configuration = merge(commonConfig, {
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 });
 
 export default prodConfig;
