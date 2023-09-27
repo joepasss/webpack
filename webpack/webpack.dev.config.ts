ec453323616fd6c249346556dc48842f854e6602
joepasss <farwes1180@gmail.com>
2023-09-28 01:25:29 +0900

    add dev server config

--- webpack/webpack.dev.config.ts
+++ webpack/webpack.dev.config.ts
@@ -1,11 +1,29 @@
-import { Configuration } from "webpack";
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 import commonConfig from "./webpack.common.config";
 import { merge } from "webpack-merge";
+import path from "path";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
 
 const devConfig: Configuration = merge(commonConfig, {
   mode: "development",
   output: {
     filename: "main.js",
+  },
+  devServer: {
+    port: 9000,
+    static: path.resolve(__dirname, "../dist"),
+    devMiddleware: {
+      index: "index.html",
+      writeToDisk: true,
+    },
+    client: {
+      overlay: true,
+    },
+    liveReload: true,
   },
   module: {
     rules: [
