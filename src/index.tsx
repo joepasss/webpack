4412bd01621253c491723ae85951ce15aa3a7b96
joepasss <farwes1180@gmail.com>
2023-11-07 22:19:04 +0900

    @feature/linters
    * install eslint packages
    
    * init eslint configs
    
    * write .sh files
    
    * implement eslint
    
    * install prettier
    
    * install eslint config prettier
    
    * install prettier plugins
    
    * implement prettier

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import App from "./App";
 import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
